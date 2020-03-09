e4cc998de0a3b5cfa53b9be60035f698c403e76c
Franco Victorio <victorio.franco@gmail.com>
2020-03-09 18:07:36 -0300

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Web3Provider, { Connectors } from 'web3-react'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MetaMask = new Connectors.InjectedConnector({
+  supportedNetworks: [4]
+})
+
+const connectors = { MetaMask }
+
+ReactDOM.render(
+  <Web3Provider connectors={connectors} libraryName='ethers.js'>
+    <App />
+    </Web3Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
