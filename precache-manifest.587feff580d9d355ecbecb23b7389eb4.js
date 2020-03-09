42be8f07503615f538a490c2ecd416d9151b7dba
Franco Victorio <victorio.franco@gmail.com>
2020-03-09 18:10:14 -0300

    Updates

---
+++ precache-manifest.587feff580d9d355ecbecb23b7389eb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d3cb34164817508bebaeecdc326c22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010d6ff26ed3c2aaacb6",
+    "url": "/static/css/main.8c2cffa3.chunk.css"
+  },
+  {
+    "revision": "7a6fbe6a8f5993edfa35",
+    "url": "/static/js/2.8dea5909.chunk.js"
+  },
+  {
+    "revision": "b2dbed9af12d18ac375c21a41a74fb5d",
+    "url": "/static/js/2.8dea5909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "010d6ff26ed3c2aaacb6",
+    "url": "/static/js/main.aa90ceb4.chunk.js"
+  },
+  {
+    "revision": "80d491ccd80332d1c748",
+    "url": "/static/js/runtime-main.333ab470.js"
+  }
+]);
